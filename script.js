557bb2af255785bfd1ec6c67ac779eb32137edd5
bbellechy <jirachaya.racha@gmail.com>
2024-06-10 10:55:36 +0700

    ลบcodeในส่วนที่ไม่ได้ใช้ และเพิ่มกรอบ

--- script.js
+++ script.js
@@ -8,19 +8,23 @@ function fetchJSONData() {
         })
         .then((data) => {
             // ส่วนรายละเอียดลูกค้า
-            const quotation_info = document.getElementById('Quotation-info');
-            const customer_info = data["Quotation"]["customer"];
+            const invoice_info = document.getElementById('invoice-info');
+            const customer_info = data["invoice"]["customer"];
             for (const info in customer_info) {
-                quotation_info.innerHTML += `${customer_info[info]}<br>`;
+                invoice_info.innerHTML += `${customer_info[info]}<br>`;
             }
-            
+
+            //ส่วนข้อมูลเอกสาร วันที่-เลขที่
+            document.getElementById('doc-date').innerHTML = `<b>วันที่ออกเอกสาร</b> ${data["invoice"]["Doc_Date"]}`;
+            document.getElementById('doc-no').innerHTML = `<b>เลขที่เอกสาร</b> ${data["invoice"]["Doc_No"]}`;
+
             //ส่วนข้อมูล payment
-            document.getElementById('customer-gr').innerHTML = data["Quotation"]["payment"]["Customer_Group"];
-            document.getElementById('note').innerHTML = data["Quotation"]["payment"]["Note"];
-            document.getElementById('bid-period').innerHTML = data["Quotation"]["payment"]["Bid period"];
-            document.getElementById('payment-term').innerHTML = data["Quotation"]["payment"]["PaymentTerm"];
+            document.getElementById('customer-gr').innerHTML = `<b>Customer Group</b> ${data["invoice"]["payment"]["Customer_Group"]}`;
+            document.getElementById('note').innerHTML = `<b>หมายเหตุ</b> ${data["invoice"]["payment"]["Note"]}`;
+            document.getElementById('bid-period').innerHTML = `<b>ระยะเวลาการเสนอราคา</b> ${data["invoice"]["payment"]["Bid period"]} วัน นับจากวันที่ออกเอกสาร`;
+            document.getElementById('payment-term').innerHTML = `<b>เงื่อนไขการชำระเงิน : </b> ${data["invoice"]["payment"]["PaymentTerm"]}`;
             // ส่วน check box
-            const payment_term = data["Quotation"]["payment"]["Term"];
+            const payment_term = data["invoice"]["payment"]["Term"];
             const checkboxes = document.querySelectorAll('input[name="payment"]'); // เลือก checkbox ทั้งหมดที่มีชื่อ "payment"
 
             checkboxes.forEach(checkbox => {
@@ -35,25 +39,39 @@ function fetchJSONData() {
 
             let rows = "";
             orders.forEach(item => {
-                rows += `<tr>
-                        <td>${item.Order_No}</td>
+                rows += `<tr style="color: blue;">
+                        <td class="text-center">${item.Order_No}</td>
                         <td>${item.Product_ID}</td>
                         <td>${item.Order}</td>
-                        <td>${item.Vat_Item}</td>
-                        <td>${item.Quantity}</td>
-                        <td>${item.Unit}</td>
-                        <td>${item.Unit_price}</td>
-                        <td>${item.Unit_discount}</td>
-                        <td>${item.Price_per_unit_after_disc}</td>
-                        <td>${item.Total_price_before_disc}</td>
-                        <td>${item.Total_discount}</td>
-                        <td>${item.Total_price_after_disc}</td>
-                        <td>${item.Vat}</td>
+                        <td class="text-center" style="width: 6%">${item.Vat_Item}</td>
+                        <td class="text-center">${item.Quantity}</td>
+                        <td class="text-center">${item.Unit}</td>
+                        <td class="text-center">${item.Unit_price}</td>
+                        <td class="text-end">${item.Unit_discount}</td>
+                        <td class="text-end">${item.Price_per_unit_after_disc}</td>
+                        <td class="text-end">${item.Total_price_before_disc}</td>
+                        <td class="text-end">${item.Total_discount}</td>
+                        <td class="text-end">${item.Total_price_after_disc}</td>
+                        <td class="text-end">${item.Vat}</td>
                         </tr>`;
                     });
 
             // เพิ่มแถวลงใน tbody
+            rows += `<td colspan="13" style="text-align: center;"> หมายเหตุ : ราคาต่อหน่วยมีขั้นต่ำต่อสถานที่ส่งตามจำนวนหน่วยที่ระบุ</td>`;
             order_list.innerHTML = rows;
+
+            // ส่วนรวมรายการ
+            const total_items = data["Orderlist"]["Total_items"];
+            const total_items_row = `
+                    <td colspan="2"><b>รวมรายการ</b></td>
+                    <td colspan="7"></td>
+                    <td class="text-end">${total_items.Total_price_before_disc}</td>
+                    <td class="text-end">${total_items.Total_discount}</td>
+                    <td class="text-end">${total_items.Total_price_after_disc}</td>
+                    <td class="text-end">${total_items.Vat}</td>
+            `;
+            // เพิ่มแถวลงใน tfoot
+            document.getElementById("total-items").innerHTML = total_items_row;
 
             //ส่วนราคารวมสินค้า
             Object.entries(data["Total Price"]).forEach(([key, value], index) => {
